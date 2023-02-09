7f7302e24a52112c4ee2ac295dc34d8ee41ee414
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2023-02-09 13:15:59 -0800

    add sayName method to Ninja class

--- main.js
+++ main.js
@@ -7,8 +7,21 @@
  */
 class Ninja {
 
-    // *** Constructor ***
-    constructor() {
+    // *** Constructor ******
+    constructor(name) {
+        this.name = name;
+        this.health = 100;
+        this.speed = 3;
+        this.strength = 3;
+    }
 
+    // *** Methods ******
+
+    /**
+     * Say Name Method
+     * Logs the ninja's name to the console
+     */
+    sayName() {
+        console.log("This ninja's name is", this.name);
     }
 }
